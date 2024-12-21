790484b2a17ebbe8f19211ddcc956d89f90b786b
Samer Zaki <Samer.Zaki22@svnu.edu.eg>
2024-12-21 20:20:21 +0200

    Question 01

--- Assignment 4/Program.cs
+++ Assignment 4/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 01
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                Console.Write(i+ ", ");
+            }
+            #endregion
         }
     }
 }
