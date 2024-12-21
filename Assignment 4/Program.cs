9c3218ab32260ace5be3286e8a551da151231339
Samer Zaki <Samer.Zaki22@svnu.edu.eg>
2024-12-21 20:28:10 +0200

    Question 02

--- Assignment 4/Program.cs
+++ Assignment 4/Program.cs
@@ -5,11 +5,19 @@
         static void Main(string[] args)
         {
             #region Question 01
-            int num = Convert.ToInt32(Console.ReadLine());
-            for (int i = 1; i <= num; i++)
-            {
-                Console.Write(i+ ", ");
-            }
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= num; i++)
+            //{
+            //    Console.Write(i+ ", ");
+            //}
+            #endregion
+
+            #region Question 02
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1*num; i <= 12*num; i+=5)
+            //{
+            //    Console.Write(i + ", ");
+            //}
             #endregion
         }
     }
